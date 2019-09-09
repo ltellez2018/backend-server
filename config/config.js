e205c4b4fc05bd6fc83d7bad693747c7f3299fdc
ltellez <ltellez.2009@gmail.com>
2019-09-09 13:16:32 -0500

    Seccion 12 Terminada Back-End

--- config/config.js
+++ config/config.js
@@ -1,1 +1,7 @@
 module.exports.SEED = '@este-es@-un-seed-hard';
+
+// * Google
+
+module.exports.GOOGLE_CLIENT_ID = '380332066665-fgu49s5l3004kabveft4ck0r5ht1lcp9.apps.googleusercontent.com';
+
+module.exports.CADUCIDAD_TOKEN = 14400;
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
-var SEED = require('../config/config').SEED;
+const SEED = require('../config/config').SEED;
+const CADUCIDAD_TOKEN = require('../config/config').CADUCIDAD_TOKEN;
+
 
 
 // * Initiation valuables
@@ -11,7 +13,114 @@ var app = express();
 //  * Exported
 var Usuario = require('../models/usuario');
 
-// * Login Service
+// * Google
+const CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+
+
+// *************  L O G I N   B Y  G O O G L E ****************** // 
+
+
+// * Verify token
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async(req, res) => {
+
+    let token = req.body.token;
+    let googleUser = await verify(token)
+        .catch(err => {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no valido'
+            });
+        });
+
+    // * SAVE IN DB
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios',
+                errors: err
+            });
+        }
+        if (usuarioDB) {
+            if (!usuarioDB.google) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar autenticacion normal'
+                });
+            } else {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: CADUCIDAD_TOKEN });
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id,
+                });
+            }
+        } else {
+            // * EL usuario no existe... se necesita crear
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+            usuario.save((err, usuarioDB) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al salvar el usuario',
+                        errors: err
+                    });
+                }
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: CADUCIDAD_TOKEN });
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id,
+                });
+
+            });
+        }
+
+    });
+
+    /*    return res.status(200).json({
+           ok: true,
+           mensaje: 'ok',
+           googleUser: googleUser
+       }); */
+
+})
+
+
+
+// *************  N O R M A L  L O G I N ****************** // 
 app.post('/', (req, res) => {
     var body = req.body;
 
@@ -42,13 +151,13 @@ app.post('/', (req, res) => {
 
         //**  T O K E N  **// 
         usuarioBD.password = ':)';
-        var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 });
+        var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: CADUCIDAD_TOKEN });
 
         res.status(200).json({
             ok: true,
             usuario: usuarioBD,
             token: token,
-            id: usuarioBD.id,
+            id: usuarioBD._id,
         });
     })
 
@@ -57,4 +166,4 @@ app.post('/', (req, res) => {
 
 
 
-module.exports = app;
+module.exports = app
